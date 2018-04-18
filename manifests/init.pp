bd8fa6f4ce6ddaceb57774856b2792bd6282fb5c
root <viktork@adaptavist.com>
2018-04-18 10:50:29 -0400

    initial commit apr 18 2018

---
+++ manifests/init.pp
@@ -0,0 +1,3 @@
+class  template_file ( $files = {} ) {
+	validate_hash($files)
+	create_resources(template_file::template_file, $files) }
---
+++ manifests/template_file.pp
@@ -0,0 +1,12 @@
+define template_file::template_file( 
+	$template_location, 
+	$file_owner, 
+    	$file_group, 
+    	$file_mode, ) { 
+        	file { $name: 
+               		content => template($template_location), 
+                   	owner => $file_owner, 
+                   	group => $file_group, 
+                   	mode => $file_mode, } } 
+
+
